50d1881d010dfa0cf4370bbc16e72e5435c94934
Mikail KAYA <kaya04baizd@gmail.com>
2021-09-23 19:52:54 +0300

    Update 01_SelectSorguları.sql

--- 01_SelectSorguları/01_SelectSorguları.sql
+++ 01_SelectSorguları/01_SelectSorguları.sql
@@ -1,4 +1,6 @@
 /*
+-----------------NORTHWIND DATABASE-----------------
+
 SELECT: SQL de SELECT ifadesi bir tablodan değer çekmek için kullanılır. 
 Örneğin Categories tablosunun bütün kolonlarını çekmek istiyoruz. O halde yazağaımız ifade aşağıdaki gibi olacaktır.
 */
@@ -11,3 +13,13 @@ SELECT: SQL de SELECT ifadesi bir tablodan değer çekmek için kullanılır.
 
 SELECT CategoryID,CategoryName -- Tablonun sadece CategoryID ve CategoryName kolonlarını çekiyoruz bu ifadeyle
 FROM Categories
+/*
+SELECT ifadesinden kolon isimleri yeri *(yıldız) ifadesini kullanıdığımız zaman bütün kolon isimlerini çekiyoruz. 
+Ancak yıldız ifadesi yerine kolon isimlerini yazarsak sadece yazdığımız kolonlar çekiliyor.
+NOT: tablomuz da kolon isimleri çok fazla ise ve bir çalışırken yani sorgumuzda bir kolonu inceleyeceksek eğer 
+kolon adını SELECT'ten hemen sonra yazarız. ve virgül koyduktan sonra tekrar *(yıldız)'ı eklersek 
+görmek istediğimiz kolon dönen tabloda en başta görünür.
+*/
+
+SELECT ShipCountry,* -- ShipCountry kolonu tablomuzda en sonda. Yandaki şekilde yazıldığında dönen tabloda en başta yer alır
+FROM Orders 
