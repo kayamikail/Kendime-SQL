8619ebad88a43bc092213146465d6b5b8f3c033e
Mikail KAYA <kaya04baizd@gmail.com>
2021-09-23 19:34:14 +0300

    Create 01_SelectSorguları.sql
    SQL de SELECT ifadesinin kullanım örnekleri.

---
+++ 01_SelectSorguları/01_SelectSorguları.sql
@@ -0,0 +1,13 @@
+/*
+SELECT: SQL de SELECT ifadesi bir tablodan değer çekmek için kullanılır. 
+Örneğin Categories tablosunun bütün kolonlarını çekmek istiyoruz. O halde yazağaımız ifade aşağıdaki gibi olacaktır.
+*/
+SELECT * FROM Categories --Tablonun bütün kolonlarını çekiyoruz bu ifadeyle
+
+/*
+SELECT: SQL de SELECT ifadesi bir tablodan değer çekmek için kullanılır. 
+Örneğin Categories tablosunun istediğimiz iki kolonunun çekmek istiyoru. O halde yazağaımız ifade aşağıdaki gibi olacaktır.
+*/
+
+SELECT CategoryID,CategoryName -- Tablonun sadece CategoryID ve CategoryName kolonlarını çekiyoruz bu ifadeyle
+FROM Categories
